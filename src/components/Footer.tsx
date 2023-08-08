9553aa128eca6ed13a7afe1316841ab65b5d5b73
yude <i@yude.jp>
2023-08-08 16:26:49 +0900

    fix: Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,19 @@
 export default function Logo() {
   return (
+    <>
     <div className="ml-5 mr-5 mt-[50px] mb-5">
       <p className="font-mono">
-        &copy; 2021 - 2023 yude. /{" "}
+        &copy; 2021 - 2023 yude /{" "}
         <a className="underline" href="https://github.com/yudejp/yude.jp">
-          GitHub
+          ソースコード (GitHub)
         </a>
       </p>
+      <div>
+        <img className="inline" src="https://moe-counter.yude.jp/get/@yude?theme=asoul" alt="表示カウンター" />
+        <img className="inline align-bottom" src="/assets/images/busy_banner.png" alt="時間ねぇ〜" />
+      </div>
     </div>
+    
+    </>
   );
 }
