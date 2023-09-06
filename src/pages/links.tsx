db7b3d68f8838f72e84f2143b726ac14491a0f69
yude <i@yude.jp>
2023-09-06 21:19:34 +0900

    fix: Update color of <code> tag

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -11,7 +11,7 @@ export default function Link() {
       <p className="mt-5 text-2xl font-serif">🔗 リンク</p>
       <hr />
 
-      <div className="prose prose-zinc mt-5 prose-headings:text-2xl prose-headings:text-center prose-headings:text-pink-100 prose-a:text-pink-100 max-w-2xl text-pink-100 prose-a:truncate">
+      <div className="prose prose-zinc mt-5 prose-headings:text-2xl prose-headings:text-center prose-headings:text-pink-100 prose-a:text-pink-100 max-w-2xl text-pink-100 prose-a:truncate prose-code:text-pink-100">
         <Doc />
       </div>
     </>
