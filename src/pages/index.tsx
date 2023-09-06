76889dbcf30b07ef8769a8ae863bb2b2f38b1ff1
yude <i@yude.jp>
2023-09-07 07:06:14 +0900

    doc: Add Spotify widget

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,17 @@ export default function Home() {
       <div className="flex justify-center">
         <QuotesWithLogo />
       </div>
+      <div>
+        <iframe
+          style={{ borderRadius: "12px" }}
+          src="https://open.spotify.com/embed/playlist/0oNsHVXi4UTVudTtvrCrKm?utm_source=generator&theme=0"
+          width="100%"
+          height="420"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
     </>
   );
 }
